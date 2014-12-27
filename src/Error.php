011253a63ca75aaf2211f96fcfe7d49e64b8b742
JulienDufresne <julien.dufresne.pro+github@gmail.com>
2014-12-27 23:11:06 +0100

    Remove the $cause parameter as it is not used

--- src/Error.php
+++ src/Error.php
@@ -1,35 +1,25 @@
 <?php
 namespace PhpDDD\Notification;
 
-use Exception;
-
 /**
  * This class is for internal purpose only
  */
 class Error
 {
-
     /**
      * @var string
      */
     private $message;
 
     /**
-     * @var Exception
-     */
-    private $cause;
-
-    /**
      * @param string         $message
-     * @param Exception|null $cause
      */
-    public function __construct($message, Exception $cause = null)
+    public function __construct($message)
     {
         if (!is_string($message)) {
             throw new InvalidArgumentException('The first parameter of Error must be a string.');
         }
         $this->message = $message;
-        $this->cause   = $cause;
     }
 
     /**
